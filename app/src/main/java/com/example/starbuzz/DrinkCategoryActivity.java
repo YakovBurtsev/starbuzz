74261ec2606529c246c4de3872f8251245669b8c
yakov <yakovburtsev@gmail.com>
2019-06-09 15:48:21 +0300

    DrinkActivity

--- app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,10 @@
 package com.example.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,5 +22,17 @@ public class DrinkCategoryActivity extends AppCompatActivity {
 
         ListView listDrinks = findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int)id);
+                startActivity(intent);
+            }
+        };
+
+        listDrinks.setOnItemClickListener(itemClickListener);
+
     }
 }
